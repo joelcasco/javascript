a5593e2f3b9c7bebe3bde261eae13af67e18c0f9
joel casco <joel_casco@hotmail.com.ar>
2022-08-18 21:40:56 -0300

    primera entraga del proyecto final

--- app.js
+++ app.js
@@ -26,17 +26,55 @@ do {
   bienvenida( valor1, operacion);
   
   //arrays
-//creo una variable para almacenar en un array los operadores
+  const historial = [valor1, operacion];
+  console.log(historial)
+
+  //creo una variable para almacenar en un array los operadores
   const operadores = ["usd", "eur", "%"];
   console.log(operadores)
-  //uso el metodo push() para agregar monedas
-  operadores.push("btc", "libra hesterlina")
-  console.log(operadores)
-  //uso el metodo pop() para quitar la el ultimo dato ya que no se usa mucho esa moneda
-  operadores.pop()
-  console.log(operadores)
+//   //uso el metodo push() para agregar monedas
+//   operadores.push("btc", "libra hesterlina")
+//   console.log(operadores)
+//   //uso el metodo pop() para quitar la el ultimo dato ya que no se usa mucho esa moneda
+//   operadores.pop()
+//   console.log(operadores)
 
 } while (confirm("desea continuar?"));
 
 alert("Gracias por utilizar este conversor")
 
+function mercado() {
+    const btc = {nombre:"btc, min: 22459 usd, max: 24883 usd, cierre: 23361 usd"}
+    const eth = {nombre: "eth, min: 1588 usd, max: 2017 usd, cierre: 1865 usd"}
+    const dai = {nombre: "dai, min: o.999 usd, max: 1.1 usd, cierre: 1 usd"}
+  
+    const listaDeMercado = [btc, eth, dai];
+// se crea la funcion
+    function consulta() {
+        const busqueda = listaDeMercado.map(resultado => resultado.nombre)
+        console.log(busqueda)
+    }
+  //se crea una funcicion
+  function IncorporarNuevaMoneda() {
+    listaDeMercado.push({
+        nombre: prompt("ingrese nombre de la moneda"),
+        historia: prompt("cuando se fundo"),
+        precioMasAlto: prompt("ingrese su pico mas elevado")
+    })
+    console.log(listaDeMercado)
+  }
+  function consultaNuevaMoneda(params) {
+    const busquedaNombre = listaDeMercado.find(resultado => resultado.nombre === prompt("ingrese el nombre de la moneda"))
+    console.log(busquedaNombre)
+  }
+  console.log(consultaNuevaMoneda)
+
+  let ingreso = Number(prompt("Analizando mercado 24/7 \n \nEscoge una opcion: \n \n1. lista de mercado \n monedas: btc,dai,eth\n \n.2 ingresa la moneda que quisieras que haya analisis"))
+  if (ingreso == 1){
+    consulta()
+}
+if(ingreso == 2){
+   IncorporarNuevaMoneda()
+}}
+
+mercado()
